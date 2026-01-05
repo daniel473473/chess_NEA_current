ede1890217e79916638f04d72c55a7ef155ea11b
Daniel <daniel_burgess@talktalk.net>
2026-01-05 11:41:17 +0000

    Add TODOs for winning move and stalemate checks
    Added TODO comments for improving game rules.

--- chess_main.py
+++ chess_main.py
@@ -11,6 +11,8 @@ import copy
 import time
 
 # TODO fix winning move check currently allows checkmates less moves
+# TODO repitiion stalemates
+# TODO insufficiant material stalemates
 
 
 def highlight_white(text):
