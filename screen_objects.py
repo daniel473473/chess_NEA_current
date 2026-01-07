238751a7e03ab64e0ed8e20d9e42a03f01c87911
Daniel <daniel_burgess@talktalk.net>
2026-01-07 12:09:08 +0000

    Add reset method and update score handling

--- screen_objects.py
+++ screen_objects.py
@@ -30,6 +30,7 @@ loading = False
 def run_loading_screen():
     global current_screen
     current_screen = loading_screen
+    current_screen.reset()
     while loading:
         current_screen.play_step()
 
@@ -85,7 +86,9 @@ class High_Score_Button(Button):
     def click(self):
         global current_screen
         current_screen = high_score_screen
+        current_screen.reset()
         current_screen.get_scores()
+        current_screen.scroll = 0
 
 
 class Game_Button(Button):
@@ -102,14 +105,16 @@ class Game_Button(Button):
         moves, boards, move_codes = game_screen.prepare_game(main_menu.depth)
         loading = False
         current_screen = game_screen
+        current_screen.reset()
         score, time = current_screen.play(moves, boards, move_codes)
         data = helper_functions.load_data(helper_functions.resource_path("high_scores.json"))
-        data["scores"].append({"score": score, "time": time})
+        data["scores"].append({"score": score, "time": time, "difficulty": main_menu.depth})
         # sort the scores from highest to lowest
         data["scores"].sort(key=lambda x: x["score"], reverse=True)
         helper_functions.store_data(helper_functions.resource_path("high_scores.json"), data)
         current_screen = play_again_screen
-        play_again_screen.update_score(score, time)
+        current_screen.reset()
+        play_again_screen.update_score(score, time, main_menu.depth)
 
 
 class Main_Menu_Button(Button):
@@ -119,6 +124,7 @@ class Main_Menu_Button(Button):
     def click(self):
         global current_screen
         current_screen = main_menu
+        current_screen.reset()
 
 
 class Slider(Button):
@@ -186,8 +192,52 @@ class Slider(Button):
         return [round((self.x_pos - self.x_base)/self.option_range) + 1, self.ID]
 
 
-class scroll_Bar(pygame.sprite.Sprite):
-    pass
+class Scroll_Bar(Button):
+    def __init__(self, x, y, x_base, y_base, x_size, y_size, x_button_size, y_button_size, base_colour, selected_colour, ID = None):
+        self.ID = ID
+        
+        
+        super().__init__(x, y, x_button_size, y_button_size, base_colour, selected_colour, "", 0)
+        
+        # add details for the actual scroll bar
+        self.x_base = x_base
+        self.y_base = y_base
+        self.x_base_size = x_size
+        self.y_base_size = y_size
+
+        self.selected = False # whether the scroll bar has been selected
+        self.scroll = 0 # ratio of how scrolled through bar is
+
+    def update(self):
+        if not self.selected:
+            super().update()
+        if self.selected:
+            y = pygame.mouse.get_pos()[1] - self.y_size * 0.5
+            if y > self.y_base and y < self.y_base + self.y_base_size:
+                self.y_pos = y
+                self.rect.y = y
+                self.image.fill(self.base_colour)
+                self.scroll = (y - self.y_base) / self.y_base_size
+            else:
+                if y < self.y_base:
+                    self.y_pos = self.y_base
+                    self.rect.y = self.y_pos
+                    self.scroll = 0
+                else:
+                    self.y_pos = self.y_base + self.y_base_size
+                    self.rect.y = self.y_pos
+                    self.scroll = 1
+
+
+    def click(self):
+        # change whether the slider is selected or not
+        self.selected = not(self.selected)
+
+        # snap the slider to the nearest option
+        if not self.selected:
+            self.image.fill(self.base_colour)
+
+        return [self.scroll, self.ID]
 
 
 class Base_Screen:
@@ -261,16 +311,29 @@ class Base_Screen:
         clock.tick(60)
 
 
+    def reset(self):
+        for slider in self.sliders:
+            slider.selected = False
+
+
 class High_Score_Screen(Base_Screen):
     def __init__(self):
         super().__init__()
         self.scroll = 0
         self.score_seperation = HEIGHT * 0.1
         self.buttons.add(Main_Menu_Button(WIDTH * 0.85, HEIGHT * 0.2, WIDTH * 0.15, HEIGHT * 0.1, (255, 0, 0), (255, 255, 0), "Main Menu"))
-        self.score_sprite = actual_screen_objects.Text_Box(WIDTH * 0.2, HEIGHT * 0.2, WIDTH * 0.6, HEIGHT * 0.7, Pac_man_colours.GRAY, "", text_size= int(HEIGHT * 0.1))
+        self.sliders.add(Scroll_Bar(WIDTH * 0.81, HEIGHT * 0.15, WIDTH * 0.85, HEIGHT * 0.15, WIDTH * 0.05, HEIGHT * 0.6, WIDTH * 0.025, HEIGHT * 0.2, Pac_man_colours.GRAY, Pac_man_colours.WHITE, ID="scroll"))
+        self.score_sprite = actual_screen_objects.Text_Box(WIDTH * 0.2, HEIGHT * 0.2, WIDTH * 0.6, HEIGHT * 0.7, Pac_man_colours.GRAY, "", text_size= int(HEIGHT * 0.075))
         self.images.add(self.score_sprite)
         self.get_scores()
         
+
+    def check_data(self):
+        for item in self.data:
+            if item[-1] == "scroll":
+                self.scroll = item[0]
+                self.get_scores()
+
 
     def get_scores(self):# load all of the high score data
         file_data = helper_functions.load_data(helper_functions.resource_path("high_scores.json"))
@@ -281,8 +344,8 @@ class High_Score_Screen(Base_Screen):
         # first reset the score sprite
         self.score_sprite.image.fill(self.score_sprite.colour)
         for i, item in enumerate(self.scores):
-            temp_text = self.score_sprite.font.render((f"{i + 1}. Score : {item['score']} Time : {item['time']}"), True, (255, 255, 255))
-            self.score_sprite.image.blit(temp_text, (WIDTH * 0.1, HEIGHT * 0.05 + self.score_seperation * i))
+            temp_text = self.score_sprite.font.render((f"{i + 1}. Score : {item['score']}, Time : {item['time']}, Difficulty : {item['difficulty']}"), True, (255, 255, 255))
+            self.score_sprite.image.blit(temp_text, (WIDTH * 0.01, HEIGHT * 0.05 + self.score_seperation * i - self.scroll * max(len(self.scores) - 6,0) * (self.score_seperation)))
 
 
 class Main_Menu_Screen(Base_Screen):
@@ -307,6 +370,7 @@ class Play_Again_Screen(Base_Screen):
         super().__init__()
         self.score = 0
         self.time = 0
+        self.difficulty = 1
         self.buttons.add(Main_Menu_Button(WIDTH * 0.2, HEIGHT * 0.8, WIDTH * 0.15, HEIGHT * 0.1, (255, 0, 0), (255, 255, 0), "Main Menu"))
         self.buttons.add(Game_Button(WIDTH * 0.5, HEIGHT * 0.8, WIDTH * 0.3, HEIGHT * 0.1, (255, 0, 0), (255, 255, 0), "Play Again"))
         self.images.add(actual_screen_objects.Text_Box(WIDTH * 0.25, HEIGHT * 0.2, WIDTH * 0.5, HEIGHT * 0.1, Pac_man_colours.BLUE, f"Game Over", int(HEIGHT * 0.1)))
@@ -314,14 +378,19 @@ class Play_Again_Screen(Base_Screen):
         self.images.add(self.score_box)
         self.time_box = actual_screen_objects.Text_Box(WIDTH * 0.6, HEIGHT * 0.5, WIDTH * 0.3, HEIGHT * 0.1, Pac_man_colours.BLUE, f"Time : {self.time}", int(HEIGHT * 0.1))
         self.images.add(self.time_box)
+        self.depth_box = actual_screen_objects.Text_Box(WIDTH * 0.35, HEIGHT * 0.35, WIDTH * 0.3, HEIGHT * 0.1, Pac_man_colours.BLUE, f"Difficulty : {self.difficulty}", int(HEIGHT * 0.1))
+        self.images.add(self.depth_box)
 
 
 
-    def update_score(self, score, time):
+
+    def update_score(self, score, time, difficulty):
         self.score = score
         self.time = time
+        self.difficulty = difficulty
         self.score_box.update_text(f"Score : {self.score}")
         self.time_box.update_text(f"Time : {self.time}")
+        self.depth_box.update_text(f"Difficulty : {self.difficulty}")
         
 
 class Loading_Screen(Base_Screen):
