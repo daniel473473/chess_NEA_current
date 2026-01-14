34ae240b4eaba068aa919ca52d0ce74b6816f3c2
Daniel <daniel_burgess@talktalk.net>
2026-01-14 11:38:14 +0000

    Refactor is_terminal_node function for accuracy

--- chess_main.py
+++ chess_main.py
@@ -123,7 +123,13 @@ def score_position(game, turn):
 
 # Function to check if a game board is a terminal node (end of the game).
 def is_terminal_node(game, moves, turn):
-    if  game.mandatory_move_delay >= 50 or (len(game.White_pieces) == 1 and len(game.Black_pieces) == 1):
+    total_pieces = game.White_pieces + game.Black_pieces
+    state = str(game)
+    if len(total_pieces) == 2 or\
+        game.mandatory_move_delay >= 50 or\
+        len(total_pieces) == 3 and ("N" in [piece.code for piece in total_pieces] or "B" in [piece.code for piece in total_pieces]) or\
+        len(total_pieces) == 4 and ("B", 1) in [(piece.code, (piece.x + piece.y) % 2) for piece in total_pieces] and ("B", 0) in [(piece.code, (piece.x + piece.y) % 2) for piece in total_pieces] or\
+        (state in self.boards and game.boards[state] == 2):# check for stalemate
         return True
     for mv in moves:
         if not mv[0] == "0":
