2f3d11527efbf684abd30b394975454324c530c1
daniel473473 <daniel_burgess@talktalk.net>
2026-01-17 11:20:43 +0000

    made win with two bishops possible

--- chess_main.py
+++ chess_main.py
@@ -128,7 +128,7 @@ def is_terminal_node(game, moves, turn):
     if len(total_pieces) == 2 or\
         game.mandatory_move_delay >= 50 or\
         len(total_pieces) == 3 and ("N" in [piece.code for piece in total_pieces] or "B" in [piece.code for piece in total_pieces]) or\
-        len(total_pieces) == 4 and ("B", 1) in [(piece.code, (piece.x + piece.y) % 2) for piece in total_pieces] and ("B", 0) in [(piece.code, (piece.x + piece.y) % 2) for piece in total_pieces] or\
+        len(total_pieces) == 4 and ("B", 1, "W") in [(piece.code, (piece.x + piece.y) % 2, piece.player) for piece in total_pieces] and ("B", 0, "B") in [(piece.code, (piece.x + piece.y) % 2, piece.player) for piece in total_pieces] or\
         (state in game.boards and game.boards[state] == 2):# check for stalemate
         return True
     for mv in moves:
